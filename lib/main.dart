d2d2956de4d0ac1f7faf8d8a70fb044ce8343ec5
Maybells <jgerbergames@gmail.com>
2020-02-01 15:36:29 -0600

    Loads images into pageview from generated urls, added textfield for entering searches

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_networkimage/provider.dart';
+import 'package:flutter_advanced_networkimage/transition.dart';
+import 'package:flutter_advanced_networkimage/zoomable.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+const String kTestString = 'Test of firebase storage';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -45,78 +32,138 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _downloaded = 'downloading...';
+  int pages = 910;
 
-  void _incrementCounter() {
+  /*void _downloadFile() async{
+    final StorageReference ref = FirebaseStorage.instance.ref().child('Firebase Test.txt');
+    final String url = await ref.getDownloadURL();
+    final http.Response downloadData = await http.get(url);
+    final Directory systemTempDir = Directory.systemTemp;
+    final File tempFile = File('${systemTempDir.path}/Firebase Test.txt');
+    if (tempFile.existsSync()) {
+      await tempFile.delete();
+    }
+    await tempFile.create();
+    assert(await tempFile.readAsString() == "");
+    final StorageFileDownloadTask task = ref.writeToFile(tempFile);
+    final int byteCount = (await task.future).totalByteCount;
+    final String tempFileContents = await tempFile.readAsString();
+    assert(tempFileContents == kTestString);
+    assert(byteCount == kTestString.length);
+
+    final String fileContents = downloadData.body;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _downloaded = fileContents;
     });
+  }*/
+
+  PageController controller;
+
+  @override
+  initState() {
+    super.initState();
+    controller = new PageController(
+      initialPage: 0,
+      keepPage: false,
+      viewportFraction: 1,
+    );
   }
 
-  StorageReference storageReference;
-  void _downloadFile() async{
-    storageReference = FirebaseStorage.instance.ref().child('Firebase Test.txt');
-    storageReference.getDownloadURL();
+  @override
+  dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  String _intToDigits(int num, int digits){
+    String out = num.toString();
+    while(out.length < digits){
+      out = "0" + out;
+    }
+    return out;
+  }
+
+  String _getImageUrl(int page) {
+    page++;
+    String filename = _intToDigits(page, 3);
+    String url = 'https://firebasestorage.googleapis.com/v0/b/bibliotheca-cd54e.appspot.com/o/Liddell-Scott%2F'+filename+'.png?alt=media';
+    return url;
+  }
+
+  void _gotoPage(int page){
+    page--;
+    if(page < 0) page = 0;
+    if(page >= pages) page = pages-1;
+    print("Going to "+page.toString());
+    controller.jumpToPage(page);
+  }
+
+  void _searchEntered(String search){
+    final numericCommand = RegExp(r'^/[0-9]+$', unicode: true);
+    if(numericCommand.hasMatch(search)){
+      search = RegExp(r'[0-9]+').stringMatch(search);
+      _gotoPage(int.parse(search));
+    }
+  }
+
+  Widget _pageviewer(){
+    return PhotoViewGallery.builder(
+      scrollPhysics: const BouncingScrollPhysics(),
+      builder: (BuildContext context, int index) {
+        return PhotoViewGalleryPageOptions(
+          imageProvider: AdvancedNetworkImage(
+              _getImageUrl(index),
+              useDiskCache: true,
+              cacheRule: CacheRule(maxAge: const Duration(days: 7)),
+              timeoutDuration: Duration(minutes: 1)
+          ),
+          initialScale: 0.0,
+          maxScale: PhotoViewComputedScale.contained * 2.0,
+          minScale: PhotoViewComputedScale.contained * 1.0,
+        );
+      },
+      itemCount: pages,
+      loadingChild: Center(
+        child: CircularProgressIndicator(),),
+      backgroundDecoration: new BoxDecoration(color: Colors.white),
+      pageController: controller,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_downloaded',
+      body: Column(
+        children: <Widget>[
+          TextField(
+            maxLines: 1,
+            onSubmitted: (text) {
+              print(text);
+              _searchEntered(text);
+            },
+            keyboardType: TextInputType.text,
+          ),
+          Expanded(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: _pageviewer(),
             ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+/*
+Expanded(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: _pageviewer(),
+                ),
+              )
+
+ */
