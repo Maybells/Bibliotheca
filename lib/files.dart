e8342c0e30aab646f93c82c262b6805ac0a216bb
Maybells <56422340+Maybells@users.noreply.github.com>
2020-05-22 12:58:41 -0700

    Improved search bar

--- lib/files.dart
+++ lib/files.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:core';
 import 'dart:io';
+import 'dart:math';
 import 'package:path_provider/path_provider.dart';
+import "package:unorm_dart/unorm_dart.dart" as unorm;
 
 enum Language{
   English,
@@ -8,15 +12,315 @@ enum Language{
   Katharevousa
 }
 
+bool isNumeric(String s) {
+  if (s == null) {
+    return false;
+  }
+  return int.tryParse(s) != null;
+}
+
 class Biblion{
   String name;
+  String id;
   Language inLang;
   Language outLang;
-  var pages;
+  Language inType;
+  Map pages;
 
   Biblion(String input){
     pages = new Map();
-    print(input);
+    pages = jsonDecode(input);
+    _loadMetadata();
+  }
+
+  void _loadMetadata(){
+    id = pages["metadata"]["id"];
+    String intype = pages["metadata"]["inType"];
+    switch(intype){
+      case "English":
+        inType = Language.English;
+        break;
+      case "Greek":
+        inType = Language.AncientGreek;
+        break;
+      default:
+        inType = Language.English;
+        break;
+    }
+  }
+
+  String _intToDigits(int num){
+    int digits = _numPages().toString().length;
+    String out = num.toString();
+    while(out.length < digits){
+      out = "0" + out;
+    }
+    return out;
+  }
+
+  int _numPages(){
+    return pages["pages"].length;
+  }
+
+  String _getPage(int page){
+    return pages["pages"][page.toString()];
+  }
+
+  bool _hasOverride(String input){
+    return pages["overrides"].containsKey(input);
+  }
+
+  String _getOverride(String input){
+    return pages["overrides"][input];
+  }
+
+  bool _isCommand(String input){
+    return input.startsWith('/');
+  }
+
+  int _doCommand(String input){
+    String command = input.substring(1);
+    if(command == 'r' || command == 'rand' || command == 'random'){
+      return Random.secure().nextInt(_numPages())+1;
+    }else if(command == 'last' || command == 'l'){
+      return _numPages();
+    }else if(command == 'first' || command == 'f'){
+      return 1;
+    } else if(isNumeric(command)){
+      return int.parse(command);
+    }else{
+      return 1;
+    }
+  }
+
+  String _toGreek(String input) {
+    String output = "";
+    for (int i = 0; i < input.length; i++) {
+      switch (input[i]) {
+        case 'a':
+          output = output + "α";
+          break;
+        case 'b':
+          output = output + "β";
+          break;
+        case 'g':
+          output = output + "γ";
+          break;
+        case 'd':
+          output = output + "δ";
+          break;
+        case 'e':
+          output = output + "ε";
+          break;
+        case 'z':
+          output = output + "ζ";
+          break;
+        case 'h':
+          output = output + "η";
+          break;
+        case 'q':
+          output = output + "θ";
+          break;
+        case 'i':
+          output = output + "ι";
+          break;
+        case 'k':
+          output = output + "κ";
+          break;
+        case 'l':
+          output = output + "λ";
+          break;
+        case 'm':
+          output = output + "μ";
+          break;
+        case 'n':
+          output = output + "ν";
+          break;
+        case 'c':
+          output = output + "ξ";
+          break;
+        case 'o':
+          output = output + "ο";
+          break;
+        case 'p':
+          output = output + "π";
+          break;
+        case 'r':
+          output = output + "ρ";
+          break;
+        case 's':
+          output = output + "σ";
+          break;
+        case 't':
+          output = output + "τ";
+          break;
+        case 'u':
+          output = output + "υ";
+          break;
+        case 'f':
+          output = output + "φ";
+          break;
+        case 'x':
+          output = output + "χ";
+          break;
+        case 'y':
+          output = output + "ψ";
+          break;
+        case 'w':
+          output = output + "ω";
+          break;
+        default:
+          output += input[i];
+          break;
+      }
+    }
+    return output;
+  }
+
+  String _toEnglish(String input) {
+    if(input.length != input.replaceAll(r'^[a-zA-Zα-ωΑ-Ω]', "").length) {
+      input = unorm.nfd(input);
+    }
+    String output = "";
+    for (int i = 0; i < input.length; i++) {
+      switch (input[i]) {
+        case 'α':
+          output = output + "a";
+          break;
+        case 'β':
+          output = output + "b";
+          break;
+        case 'γ':
+          output = output + "g";
+          break;
+        case 'δ':
+          output = output + "d";
+          break;
+        case 'ε':
+          output = output + "e";
+          break;
+        case 'ζ':
+          output = output + "z";
+          break;
+        case 'η':
+          output = output + "h";
+          break;
+        case 'θ':
+          output = output + "q";
+          break;
+        case 'ι':
+          output = output + "i";
+          break;
+        case 'κ':
+          output = output + "k";
+          break;
+        case 'λ':
+          output = output + "l";
+          break;
+        case 'μ':
+          output = output + "m";
+          break;
+        case 'ν':
+          output = output + "n";
+          break;
+        case 'ξ':
+          output = output + "c";
+          break;
+        case 'ο':
+          output = output + "o";
+          break;
+        case 'π':
+          output = output + "p";
+          break;
+        case 'ρ':
+          output = output + "r";
+          break;
+        case 'σ':
+          output = output + "s";
+          break;
+        case 'ς':
+          output = output + "s";
+          break;
+        case 'τ':
+          output = output + "t";
+          break;
+        case 'υ':
+          output = output + "u";
+          break;
+        case 'φ':
+          output = output + "f";
+          break;
+        case 'χ':
+          output = output + "x";
+          break;
+        case 'ψ':
+          output = output + "y";
+          break;
+        case 'ω':
+          output = output + "w";
+          break;
+        default:
+          output += input[i];
+          break;
+      }
+    }
+    return output;
+  }
+
+  String _convertString(String input){
+    input = input.toLowerCase();
+    input = input.replaceAll(r"[\p{InCombiningDiacriticalMarks}]", "");
+    switch(inType){
+      case Language.English:
+        return _toEnglish(input);
+        break;
+      case Language.AncientGreek:
+        return _toGreek(input);
+        break;
+      default:
+        return input;
+        break;
+    }
+  }
+
+  _dictionarySearch(String input, [int lower = 1, int upper = -1]){
+    if(upper == -1){
+      upper = _numPages();
+    }
+    if(lower >= upper){
+      return lower + 1;
+    }
+
+    int mid = (upper + lower) ~/ 2;
+    if(mid == lower){
+      return mid + 1;
+    }
+    String current = _getPage(mid);
+    if(input.compareTo(current) < 0){
+      return _dictionarySearch(input, lower, mid);
+    }else if(input.compareTo(current) > 0){
+      return _dictionarySearch(input, mid, upper);
+    }else{
+      return mid;
+    }
+  }
+
+
+
+  Future<int> search(String input) async{
+    if(_hasOverride(input)){
+      return search(_getOverride(input));
+    }else if(_isCommand(input)){
+      return _doCommand(input);
+    }else{
+      input = _convertString(input);
+      return _dictionarySearch(input);
+    }
+  }
+
+  String getUrl(int page){
+    String filename =_intToDigits(page);
+    return 'https://firebasestorage.googleapis.com/v0/b/bibliotheca-cd54e.appspot.com/o/$id%2F$filename.png?alt=media';
   }
 }
 
@@ -29,7 +333,7 @@ class FileLoader{
 
   Future<File> _localFile(String name) async {
     final path = await _localPath;
-    return File('$path/$name.txt');
+    return File('$path/$name.json');
   }
 
   Future<String> readFile(String name) async {
@@ -44,5 +348,12 @@ class FileLoader{
       // If encountering an error, return 0
       return null;
     }
+  }
+
+  Future<File> writeFile(String string) async {
+    final file = await _localFile(string);
+
+    // Write the file
+    return file.writeAsString(string);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'files.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_networkimage/provider.dart';
 import 'package:flutter_advanced_networkimage/transition.dart';
 import 'package:flutter_advanced_networkimage/zoomable.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 const String kTestString = 'Test of firebase storage';
 
@@ -33,6 +35,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int pages = 910;
+  bool _searching = false;
+  String _currentBiblion;
 
   /*void _downloadFile() async{
     final StorageReference ref = FirebaseStorage.instance.ref().child('Firebase Test.txt');
@@ -58,6 +62,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }*/
 
   PageController controller;
+  TextEditingController textController;
+  Biblion _biblion;
 
   @override
   initState() {
@@ -67,6 +73,9 @@ class _MyHomePageState extends State<MyHomePage> {
       keepPage: false,
       viewportFraction: 1,
     );
+    textController = new TextEditingController();
+    _biblion = null;
+    _loadBiblion("Meletontas");
   }
 
   @override
@@ -75,59 +84,73 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  String _intToDigits(int num, int digits){
-    String out = num.toString();
-    while(out.length < digits){
-      out = "0" + out;
-    }
-    return out;
+  void _loadBiblion(String name) async {
+    _currentBiblion = name;
+//    FileLoader loader = new FileLoader();
+//    String contents = await loader.readFile(name);
+//    String contents = await rootBundle.loadString('assets/$name.json');
+    String contents = await DefaultAssetBundle.of(context)
+        .loadString('lib/assets/$name.json');
+    setState(() {
+      _biblion = new Biblion(contents);
+    });
   }
 
   String _getImageUrl(int page) {
     page++;
-    String filename = _intToDigits(page, 3);
-    String url = 'https://firebasestorage.googleapis.com/v0/b/bibliotheca-cd54e.appspot.com/o/Liddell-Scott%2F'+filename+'.png?alt=media';
+    String url = _biblion.getUrl(page);
     return url;
   }
 
-  void _gotoPage(int page){
+  void _gotoPage(int page) {
     page--;
-    if(page < 0) page = 0;
-    if(page >= pages) page = pages-1;
-    print("Going to "+page.toString());
+    if (page < 0) page = 0;
+    if (page >= pages) page = pages - 1;
+    print("Going to " + page.toString());
     controller.jumpToPage(page);
   }
 
-  void _searchEntered(String search){
-    final numericCommand = RegExp(r'^/[0-9]+$', unicode: true);
-    if(numericCommand.hasMatch(search)){
-      search = RegExp(r'[0-9]+').stringMatch(search);
-      _gotoPage(int.parse(search));
-    }
+  void _searchFor(String input) async {
+    _gotoPage(await _biblion.search(input));
+    setState(() {
+      _searching = false;
+    });
   }
 
-  Widget _pageviewer(){
-    return PhotoViewGallery.builder(
-      scrollPhysics: const BouncingScrollPhysics(),
-      builder: (BuildContext context, int index) {
-        return PhotoViewGalleryPageOptions(
-          imageProvider: AdvancedNetworkImage(
-              _getImageUrl(index),
-              useDiskCache: true,
-              cacheRule: CacheRule(maxAge: const Duration(days: 7)),
-              timeoutDuration: Duration(minutes: 1)
-          ),
-          initialScale: 0.0,
-          maxScale: PhotoViewComputedScale.contained * 2.0,
-          minScale: PhotoViewComputedScale.contained * 1.0,
-        );
-      },
-      itemCount: pages,
-      loadingChild: Center(
-        child: CircularProgressIndicator(),),
-      backgroundDecoration: new BoxDecoration(color: Colors.white),
-      pageController: controller,
-    );
+  void _searchEntered(String search) async {
+    if (_biblion != null) {
+      _searchFor(search);
+    }
+    textController.clear();
+  }
+
+  Widget _pageviewer() {
+    if (_biblion != null && !_searching) {
+      return PhotoViewGallery.builder(
+        scrollPhysics: const BouncingScrollPhysics(),
+        builder: (BuildContext context, int index) {
+          return PhotoViewGalleryPageOptions(
+            imageProvider: AdvancedNetworkImage(_getImageUrl(index),
+                useDiskCache: true,
+                cacheRule: CacheRule(maxAge: const Duration(days: 7)),
+                timeoutDuration: Duration(minutes: 1)),
+            initialScale: 0.0,
+            maxScale: PhotoViewComputedScale.contained * 2.0,
+            minScale: PhotoViewComputedScale.contained * 1.0,
+          );
+        },
+        itemCount: pages,
+        loadingChild: Center(
+          child: CircularProgressIndicator(),
+        ),
+        backgroundDecoration: new BoxDecoration(color: Colors.white),
+        pageController: controller,
+      );
+    } else {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
   }
 
   @override
@@ -136,16 +159,73 @@ class _MyHomePageState extends State<MyHomePage> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+//          IconButton(
+//            icon: const Icon(Icons.list),
+//            tooltip: 'Choose Book',
+//            onPressed: (){
+//              showDialog(
+//                context: context,
+//                barrierDismissible: true,
+//                builder: (BuildContext context){
+//                  return AlertDialog(
+//                    title: Text('Choose Book'),
+//                    content: SingleChildScrollView(
+//                      child: ,
+//                    ),
+//                  );
+//                }
+//              );
+//            },
+//          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
-          TextField(
-            maxLines: 1,
-            onSubmitted: (text) {
-              print(text);
-              _searchEntered(text);
-            },
-            keyboardType: TextInputType.text,
+          Container(
+            margin: EdgeInsets.only(bottom: 5.0),
+            padding: EdgeInsets.symmetric(horizontal: 5.0),
+            child: Center(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.library_books),
+                    tooltip: 'Switch books',
+                    iconSize: 28.0,
+                    color: Colors.grey,
+                    onPressed: () => showBookPicker(context),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextField(
+                        textAlignVertical: TextAlignVertical.bottom,
+                        maxLines: 1,
+                        minLines: 1,
+                        style: TextStyle(fontSize: 18.0),
+                        controller: textController,
+                        onSubmitted: (text) {
+                          setState(() {
+                            _searching = true;
+                          });
+                          _searchEntered(text);
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.search),
+                          hintText: 'Search...',
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.history),
+                    iconSize: 28.0,
+                    tooltip: 'Search history',
+                  ),
+                ],
+              ),
+            ),
           ),
           Expanded(
             child: SizedBox(
@@ -157,13 +237,46 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-/*
-Expanded(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: _pageviewer(),
-                ),
-              )
 
- */
+  changeBook(String to){
+    setState(() {
+      _currentBiblion = to;
+      _loadBiblion(to);
+    });
+  }
+
+  showBookPicker(BuildContext context){
+    Widget optionOne = SimpleDialogOption(
+      child: const Text('Liddell-Scott'),
+      onPressed: () {
+        print('Liddell-Scott');
+        changeBook('Liddell-Scott');
+        Navigator.of(context).pop();
+      },
+    );
+    Widget optionTwo = SimpleDialogOption(
+      child: const Text('Meletontas'),
+      onPressed: () {
+        print('Meletontas');
+        changeBook('Meletontas');
+        Navigator.of(context).pop();
+      },
+    );
+
+    SimpleDialog dialog = SimpleDialog(
+      title: const Text('Choose a book'),
+      children: <Widget>[
+        optionOne,
+        optionTwo,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return dialog;
+      },
+    );
+  }
+}
